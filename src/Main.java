9633f0deab97f0f2ecfc38a7a2775f765e2f151b
FabrizioMisseri <misserifabrizio@gmail.com>
2023-03-23 14:32:01 +0100

    milestone-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        SELECT countries.name, countries.country_id, regions.name, continents.name FROM `countries`
+//        INNER JOIN regions on countries.region_id = regions.region_id
+//        INNER JOIN continents on regions.continent_id = continents.continent_id
+//        ORDER BY countries.name;
+
     }
 }
