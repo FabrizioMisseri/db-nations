b9bf30c4666ec587a94bc0a45933bb7226e66cff
FabrizioMisseri <misserifabrizio@gmail.com>
2023-03-23 15:16:04 +0100

    milestone-2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,44 @@
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
 //        SELECT countries.name, countries.country_id, regions.name, continents.name FROM `countries`
 //        INNER JOIN regions on countries.region_id = regions.region_id
 //        INNER JOIN continents on regions.continent_id = continents.continent_id
 //        ORDER BY countries.name;
+        String url = "jdbc:mysql://localhost:3306/db-nations";
+        String user = "root";
+        String password = "root";
+
+        try (Connection con = DriverManager.getConnection(url, user, password)){
+            //System.out.println("ciao");
+            String query = """
+                    SELECT countries.name, countries.country_id, regions.name, continents.name FROM `countries`
+                    INNER JOIN regions on countries.region_id = regions.region_id
+                    INNER JOIN continents on regions.continent_id = continents.continent_id
+                    ORDER BY countries.name;
+                    """;
+            try(PreparedStatement ps = con.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY)){
+                try(ResultSet rs = ps.executeQuery()){
+                    String outputString = "";
+
+                    while (rs.next()){
+                        String nameCountry = rs.getString(1);
+                        Integer countryId = rs.getInt(2);
+                        String nameRegion = rs.getString(3);
+                        String nameContinent = rs.getString(4);
+
+                        outputString += nameCountry +" "+ countryId +" "+ nameRegion +" "+ nameContinent +" "+ "\n";
+                    }
+
+                    System.out.println(outputString);
+
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
